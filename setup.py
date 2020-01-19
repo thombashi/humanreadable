856ff2a9398d5453aff5ab36f786e7e4962be774
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-19 11:25:18 +0900

    Add long_description_content_type

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setuptools.setup(
     keywords=["human-readable", "converter"],
     license=pkg_info["__license__"],
     long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/x-rst",
     packages=setuptools.find_packages(exclude=["test*"]),
     project_urls={
         "Source": REPOSITORY_URL,
