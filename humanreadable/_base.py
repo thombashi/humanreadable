e2a2f20a57c0c8e63f8d0faa469aa7ed381629b6
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-02-08 02:04:54 +0900

    Remove an attribute

--- humanreadable/_base.py
+++ humanreadable/_base.py
@@ -39,11 +39,8 @@ class HumanReadableValue(object):
         pass
 
     def __init__(self, readable_value, default_unit=None):
-        self._readable_value = readable_value
-        self._number = None
-        self._from_unit = None
         self._default_unit = default_unit
-        self.__preprocess()
+        self._number, self._from_unit = self.__preprocess(readable_value)
 
     def __repr__(self):
         items = [six.text_type(self._number)]
@@ -52,21 +49,21 @@ class HumanReadableValue(object):
 
         return " ".join(items)
 
-    def __split_unit(self):
-        if RealNumber(self._readable_value).is_type():
+    def __split_unit(self, readable_value):
+        if RealNumber(readable_value).is_type():
             if self._default_unit is None:
                 raise UnitNotFoundError(
                     "unit not found",
-                    value=self._readable_value,
+                    value=readable_value,
                     available_units=_get_unit_msg(self._text_units),
                 )
 
-            return (self._readable_value, self._default_unit)
+            return (readable_value, self._default_unit)
 
         for unit in self._units:
             try:
-                if unit.regexp.search(self._readable_value):
-                    number = unit.regexp.split(self._readable_value)[0]
+                if unit.regexp.search(readable_value):
+                    number = unit.regexp.split(readable_value)[0]
                     if not RealNumber(number).is_type():
                         continue
 
@@ -74,22 +71,24 @@ class HumanReadableValue(object):
             except TypeError:
                 continue
 
-        return (self._readable_value, self._default_unit)
+        return (readable_value, self._default_unit)
 
-    def __preprocess(self):
-        if self._readable_value is None:
+    def __preprocess(self, readable_value):
+        if readable_value is None:
             raise TypeError("readable_value must be a string")
 
-        readable_num, self._from_unit = self.__split_unit()
-        if readable_num is not None:
-            self._number = self.__to_number(readable_num)
+        number, from_unit = self.__split_unit(readable_value)
+        if number is not None:
+            number = self.__to_number(number)
 
-        if self._from_unit is None:
+        if from_unit is None:
             raise UnitNotFoundError(
                 "unit not found",
-                value=self._readable_value,
+                value=readable_value,
                 available_units=_get_unit_msg(self._text_units),
             )
+
+        return (number, from_unit)
 
     def __to_number(self, readable_num):
         match = _RE_NUMBER.search(readable_num)
