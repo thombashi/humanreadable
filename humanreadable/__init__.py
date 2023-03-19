74a6adce1dfaa057a1df50e59ac5673f6bf43e5a
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-03-19 23:35:55 +0900

    Add __all__

--- humanreadable/__init__.py
+++ humanreadable/__init__.py
@@ -2,3 +2,16 @@ from .__version__ import __author__, __copyright__, __email__, __license__, __ve
 from ._persec import BitPerSecond
 from ._time import Time
 from .error import ParameterError, UnitNotFoundError
+
+
+__all__ = (  # type: ignore
+    "__author__",
+    "__copyright__",
+    "__email__",
+    "__license__",
+    "__version__",
+    BitPerSecond,
+    Time,
+    ParameterError,
+    UnitNotFoundError,
+)
